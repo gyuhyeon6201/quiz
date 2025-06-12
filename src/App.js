2bd96a5831f910b57cba3c40dcc9ec69a71a4a7c
gyuhyeon6201 <rbgus303@gmail.com>
2025-06-12 17:11:31 +0900

    create react quiz project (0612)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+// 컴포넌트
+import quizData from "./data/quizData";
+import Categoris from "./Components/Categoris";
+import QuizPage from "./Components/QuizPage";
+import Results from "./Components/Results";
 
 const App = () => {
+  const [category, setCategory] = useState("");
+  const [filterQuiz, setFilterQuiz] = useState([]);
+  const [finished, setFinished] = useState(false);
+  const [score, setScore] = useState(0);
+  const onSelectCategory = (select)=>{
+    setCategory(select);
+    
+    const quizArr = quizData.quizzes.filter((data)=>{
+      return data.category === select;
+    });
+    setFilterQuiz(quizArr);
+  }
+  const handleFinish = (score)=>{
+    setFinished(true);
+    setScore(score);
+  }
+  const handleRestart =()=>{
+    setCategory('');
+    setFinished(false);
+  }
   return (
-    <div>
-      
+    <div className="app">
+      {!category && !finished&&(
+        <Categoris categoris={quizData.categories}
+                 onSelect={onSelectCategory}/>
+      )}
+      {
+        category && !finished &&(<QuizPage quiz={filterQuiz} onFinished={handleFinish}/>
+      )}
+      {
+        finished && <Results onRestart ={handleRestart} appScore={score}/>
+      }
     </div>
   );
 };
